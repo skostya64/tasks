7682588dc34cc37de945bb06717da086e856a713
skostya64 <skostya64@gmail.com>
2017-12-04 22:10:05 +0200

    finish

--- checking_data_type.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-r = requests.get('http://127.0.0.1:5000/todo/api/v1.0/tasks')
-
-assert type(r.json()['tasks'][0]['description']) == str
-assert type(r.json()['tasks'][0]['done']) == bool
-assert type(r.json()['tasks'][0]['id']) == int
-assert type(r.json()['tasks'][0]['title']) == str
--- tests/test_check_types_data.py
+++ tests/test_check_types_data.py
@@ -1,12 +1,10 @@
 import requests
 
 
-r = requests.get('http://127.0.0.1:5000/todo/api/v1.0/tasks')
-
-
 def test_check_types_data():
     r = requests.get('http://127.0.0.1:5000/todo/api/v1.0/tasks')
-    assert type(r.json()['tasks'][0]['description']) == str
-    assert type(r.json()['tasks'][0]['done']) == bool
-    assert type(r.json()['tasks'][0]['id']) == int
-    assert type(r.json()['tasks'][0]['title']) == str
+    for i in r.json()['tasks']:
+        assert type(i['description']) == str
+        assert type(i['done']) == bool
+        assert type(i['id']) == int
+        assert type(i['title']) == str
