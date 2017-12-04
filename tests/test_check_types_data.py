c0660be16ae37984cc47aafbcd02ca4cf29dc0b6
skostya64 <skostya64@gmail.com>
2017-12-04 22:22:08 +0200

    finish1

--- tests/test_check_types_data.py
+++ tests/test_check_types_data.py
@@ -4,7 +4,7 @@ import requests
 def test_check_types_data():
     r = requests.get('http://127.0.0.1:5000/todo/api/v1.0/tasks')
     for i in r.json()['tasks']:
-        assert type(i['description']) == str
-        assert type(i['done']) == bool
-        assert type(i['id']) == int
-        assert type(i['title']) == str
+         assert type(i['description']) == str
+         assert type(i['done']) == bool
+         assert type(i['id']) == int
+         assert type(i['title']) == str
