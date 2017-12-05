75a631f14f90471b0caa51aca717562d9f7e3cf2
skostya64 <skostya64@gmail.com>
2017-12-05 18:32:55 +0200

    finish2

--- tests/test_check_types_data.py
+++ tests/test_check_types_data.py
@@ -1,10 +1,23 @@
 import requests
 
 
+# def test_check_types_data():
+#     r = requests.get('http://127.0.0.1:5000/todo/api/v1.0/tasks')
+#     for i in r.json()['tasks']:
+#          assert type(i['description']) == str
+#          assert type(i['done']) == bool
+#          assert type(i['id']) == int
+#          assert type(i['title']) == str
+
+
 def test_check_types_data():
     r = requests.get('http://127.0.0.1:5000/todo/api/v1.0/tasks')
-    for i in r.json()['tasks']:
-         assert type(i['description']) == str
-         assert type(i['done']) == bool
-         assert type(i['id']) == int
-         assert type(i['title']) == str
+    for i in range(len(r.json()['tasks'])):
+        assert type(r.json()['tasks'][i]['description']) == str
+        assert type(r.json()['tasks'][i]['done']) == bool
+        assert type(r.json()['tasks'][i]['id']) == int
+        assert type(r.json()['tasks'][i]['title']) == str
+
+
+
+
